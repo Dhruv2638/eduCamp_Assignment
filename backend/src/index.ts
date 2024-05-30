4b8d6bb5acb8801371df157a8a31afd7619e5b6c
DHRUV NAGAR <you@example.com>
2024-05-30 23:21:47 +0530

    DEV: config code added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,14 @@
 // src/index.ts
-import express from 'express';
+import express from "express";
+import { envConfig } from "./config/envconfig";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(envConfig.server.PORT, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
